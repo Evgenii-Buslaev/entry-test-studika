618377fb03f4ff44bb660ad9221fe605a62d4120
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-12-09 17:52:25 +0500

    Created function to get cities

---
+++ index.js
@@ -0,0 +1,16 @@
+const API_URL = "https://studika.ru/api/areas";
+const options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+};
+
+const citiesFetching = async () => {
+  const response = await fetch(API_URL, options);
+  const cities = await response.json();
+  console.log(cities);
+  return cities;
+};
+
+const cities = citiesFetching();
