25849c1303f4514c7508ff0bb289b8939a62ec00
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-12-10 14:02:25 +0500

    created datalist with cities

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-import { menuBtn, closeBtn, toggler } from "./scripts/dynamic-styles.js";
+import { menuBtn, closeBtn, toggler } from "./scripts/toggleMenu.js";
 import { citiesFetching } from "./scripts/get-cities.js";
-
-console.log(toggler);
-console.log(menuBtn);
 
 menuBtn.addEventListener("click", toggler);
 closeBtn.addEventListener("click", toggler);
